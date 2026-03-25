44747efb3744a12ffda48ef783ddd745e2ba08d4
tim <tfangmeyer@gmail.com>
2026-03-25 23:45:08 +0100

    Refine storytelling map presentation

--- storytelling/config.js
+++ storytelling/config.js
@@ -1,11 +1,36 @@
 var config = {
-  style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+  style: {
+    version: 8,
+    glyphs: "https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf",
+    sources: {
+      carto: {
+        type: "raster",
+        tiles: ["https://a.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png"],
+        tileSize: 256,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; CARTO',
+      },
+    },
+    layers: [
+      {
+        id: "background",
+        type: "background",
+        paint: { "background-color": "#0b0d0f" },
+      },
+      {
+        id: "carto",
+        type: "raster",
+        source: "carto",
+        paint: { "raster-opacity": 0.88 },
+      },
+    ],
+  },
   showMarkers: false,
   inset: false,
   theme: "dark",
   use3dTerrain: false,
   auto: false,
-  title: "🧬 Berlins DNA in Straßennamen",
+  title: "Berlins DNA in Straßennamen",
   byline: "",
   footer:
     'Datenquelle: <a href="https://daten.berlin.de/datensaetze/stra%C3%9Fenabschnitte-berlin" target="_blank">Straßenabschnitte RBS Berlin</a> (Amt für Statistik Berlin-Brandenburg, CC-BY-3.0) <br> Created using <a href="https://maplibre.org/maplibre-gl-js/docs/" target="_blank">MapLibre GL JS</a> + <a href="https://github.com/russellsamora/scrollama" target="_blank">Scrollama</a>.<br></br>by Sören Etler | <a href="https://soerenetler.de/de/page/impressum/" target="_blank">Impressum</a>',
@@ -13,11 +38,11 @@ var config = {
     {
       id: "intro",
       alignment: "center",
+      fitToStreetBounds: false,
       hidden: false,
-      description:
-        'Straßennamen beschreiben nicht nur die Orte um uns herum, sondern bilden auch die kollektive DNA einer Stadt. Das gilt nirgends mehr als in Berlin. Hier schichten sich Geschichte auf Geschichte: preußische Königsstraßen, kaiserliche Triumphachsen, nationalsozialistische Umbenennungen, DDR-Prachtboulevards und der anhaltende Kampf um das Erinnern. Man kann sehr lange in Berlin leben, ohne Straßennamen zu kennen. Aber wenn man mit „Einheimischen" spricht, ist man immer wieder beeindruckt, wie fließend sie Orte anhand von Straßennamen beschreiben können.',
+      description: "",
       location: {
-        center: [13.405, 52.52],
+        center: [13.398058, 52.520545],
         zoom: 10.5,
         pitch: 20,
         bearing: 0,
@@ -51,16 +76,36 @@ var config = {
       ],
     },
     {
+      id: "dna-der-stadt",
+      alignment: "center",
+      fitToStreetBounds: false,
+      hidden: false,
+      description:
+        'Straßennamen beschreiben nicht nur die Orte um uns herum, sondern bilden auch die kollektive DNA einer Stadt. Das gilt nirgends mehr als in Berlin. Hier schichten sich Geschichte auf Geschichte: preußische Königsstraßen, kaiserliche Triumphachsen, nationalsozialistische Umbenennungen, DDR-Prachtboulevards und der anhaltende Kampf um das Erinnern. Man kann sehr lange in Berlin leben, ohne Straßennamen zu kennen. Aber wenn man mit „Einheimischen" spricht, ist man immer wieder beeindruckt, wie fließend sie Orte anhand von Straßennamen beschreiben können.',
+      location: {
+        center: [13.398058, 52.520545],
+        zoom: 10.5,
+        pitch: 20,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterExit: [],
+    },
+    {
       id: "adlergestell",
       alignment: "left",
+      fitToStreetBounds: false,
       hidden: false,
       title: "Adlergestell",
       // image: './assets/adlergestell.jpg',
       description:
-        "Im Berliner Straßenregister ist das <b>Adlergestell</b> mit rund <b>13,3 Kilometern</b> die längste zusammenhängend erfasste Straße. Sie verläuft durch <b>Treptow-Köpenick</b> und macht sichtbar, wie weit sich Berlin im Südosten ausdehnt. In den verknüpften Berliner Adressdaten hängen <b>212 Adressen</b> an dieser Straße; das entspricht <b>Rang 85</b> unter den Straßen der Stadt. Das ist wohlgemerkt ein Adress-Ranking, kein direktes Maß für Einwohnerinnen und Einwohner.",
+        "Im Berliner Straßenregister ist das <b>Adlergestell</b> mit rund <b>13,3 Kilometern</b> die längste zusammenhängend erfasste Straße. Sie verläuft durch <b>Treptow-Köpenick</b> und macht sichtbar, wie weit sich Berlin im Südosten ausdehnt. In den verknüpften Berliner Adressdaten hängen <b>212 Adressen</b> an dieser Straße; das entspricht <b>Rang 85</b> unter den Straßen der Stadt.",
       location: {
-        center: [13.535, 52.44],
-        zoom: 12,
+        center: [13.604511, 52.402091],
+        zoom: 11.38,
         pitch: 20,
         bearing: 0,
       },
@@ -88,9 +133,9 @@ var config = {
       hidden: false,
       title: "Die kürzeste Straße",
       description:
-        "Ein Berliner Kuriosum liegt im Nikolaiviertel: Die <b>Eiergasse</b> gilt mit nur rund <b>16 Metern</b> als kürzeste Straße der Stadt. Sie verbindet den Nikolaikirchplatz mit dem Mühlendamm und dient heute vor allem als schmale Passage mit Gastronomie-Außenflächen. Auch andere Namen im Register wirken wie kleine Verwaltungsfundstücke – etwa <b>Weg A</b> bis <b>Weg G</b> oder der mittelalterliche <b>Kolk</b> in Spandau. Doch die Eiergasse zeigt am deutlichsten, wie selbst winzige Straßen zu Teilchen der Berliner Stadtgeschichte werden.",
+        "Ein Berliner Kuriosum liegt im Nikolaiviertel: Die <b>Eiergasse</b> gilt mit nur rund <b>16 Metern</b> als kürzeste Straße der Stadt. Sie verbindet den Nikolaikirchplatz mit dem Mühlendamm und dient heute vor allem als schmale Passage mit Gastronomie-Außenflächen.",
       location: {
-        center: [13.201, 52.539],
+        center: [13.408438, 52.516738],
         zoom: 17,
         pitch: 20,
         bearing: 0,
@@ -99,6 +144,11 @@ var config = {
       rotateAnimation: false,
       callback: "",
       onChapterEnter: [
+        {
+          layer: "all-streets-context-thin",
+          opacity: 0.34,
+          duration: 900,
+        },
         {
           layer: "eiergasse",
           opacity: 1,
@@ -111,6 +161,11 @@ var config = {
         },
       ],
       onChapterExit: [
+        {
+          layer: "all-streets-context-thin",
+          opacity: 0,
+          duration: 600,
+        },
         {
           layer: "eiergasse",
           opacity: 0,
@@ -126,14 +181,15 @@ var config = {
     {
       id: "sonnenallee",
       alignment: "right",
+      fitToStreetBounds: false,
       hidden: false,
       title: "Sonnenallee",
       // image: './assets/sonnenallee.jpg',
       description:
-        "Die <b>Sonnenallee</b> gehört zu den berühmtesten Straßen Berlins – nicht nur wegen des gleichnamigen Films über jenen kurzen Abschnitt, der in der DDR lag. Das Partyvolk fand und findet hier angesagte Locations, die vielen kleinen Lebensmittelgeschäfte erfüllen auch außergewöhnliche Wünsche. Vom Hermannplatz bis zum Baumschulenweg sind es knapp fünf Kilometer – angefangen hat alles ganz klein: 1880 auf dem sumpfigen Gebiet des damals eigenständigen Rixdorfs mit dem schlichten Namen Straße 84.",
+        "Die <b>Sonnenallee</b> gehört zu den berühmtesten Straßen Berlins – nicht nur wegen des gleichnamigen Films über jenen kurzen Abschnitt, der in der DDR lag. Das Partyvolk fand und findet hier angesagte Locations, die vielen kleinen Lebensmittelgeschäfte erfüllen auch außergewöhnliche Wünsche. Vom Hermannplatz bis zum Baumschulenweg sind es knapp fünf Kilometer.",
       location: {
-        center: [13.447, 52.476],
-        zoom: 13,
+        center: [13.459518, 52.471513],
+        zoom: 12.81,
         pitch: 20,
         bearing: 0,
       },
@@ -186,46 +242,17 @@ var config = {
           duration: 5000,
         },
       ],
-      id: "landsberger-allee",
-      alignment: "right",
-      hidden: false,
-      title: "Landsberger Allee",
-      description:
-        "Die <b>Landsberger Allee</b> verweist auf das frühere <b>Landsberg an der Warthe</b>, heute <b>Gorzów Wielkopolski</b> in Polen. Straßennamen konservieren Geographien, die politisch verschwunden sind, auf Berliner Schildern aber weiterleben.",
-      location: {
-        center: [13.468, 52.527],
-        zoom: 12,
-        pitch: 20,
-        bearing: 0,
-      },
-      mapAnimation: "flyTo",
-      rotateAnimation: false,
-      callback: "",
-      onChapterEnter: [
-        {
-          layer: "landsberger-allee",
-          opacity: 1,
-          duration: 5000,
-        },
-      ],
-      onChapterExit: [
-        {
-          layer: "landsberger-allee",
-          opacity: 0,
-          duration: 5000,
-        },
-      ],
     },
     {
       id: "kietz",
       alignment: "right",
       hidden: false,
-      title: "Kietz",
+      title: "Kiez",
       description:
-        "Berlin ist ein Millionendorf – und trägt die Dörfer noch in sich. Historisch hieß es <b>Kietz</b>: So wurden slawische Siedlungen am Rand mittelalterlicher Städte bezeichnet, die schon vor der brandenburgischen Expansion bestanden. Im Berliner Alltagsdeutsch wurde daraus später <b>Kiez</b> – nicht mehr als eigener Siedlungstyp, sondern als Wort für das vertraute Wohnviertel. Im Berliner Straßenregister taucht <b>Kietz/Kiez</b> heute noch in <b>10</b> Straßennamen auf, vom schlichten <b>Kietz</b> bis zum neueren <b>Möckernkiez</b>.",
+        "Berlin ist ein Millionendorf – und trägt die Dörfer noch in sich. Historisch hieß es <b>Kietz</b>: So wurden slawische Siedlungen am Rand mittelalterlicher Städte bezeichnet, die schon vor der brandenburgischen Expansion bestanden. Im Berliner Alltagsdeutsch wurde daraus später <b>Kiez</b> – nicht mehr als eigener Siedlungstyp, sondern als Wort für das vertraute Wohnviertel. Im Berliner Straßenregister taucht <b>Kietz/Kiez</b> heute noch in <b>10</b> Straßennamen auf. Hier reicht ein einziges Beispiel: <b>Möckernkiez</b>.",
       location: {
-        center: [13.578, 52.443],
-        zoom: 15,
+        center: [13.378016, 52.493111],
+        zoom: 16.1,
         pitch: 20,
         bearing: 0,
       },
@@ -238,10 +265,20 @@ var config = {
           opacity: 1,
           duration: 5000,
         },
+        {
+          layer: "moeckernkiez",
+          opacity: 1,
+          duration: 5000,
+        },
       ],
       onChapterExit: [
         {
           layer: "satellite-imagery",
+          opacity: 0,
+          duration: 5000,
+        },
+        {
+          layer: "moeckernkiez",
           opacity: 0,
           duration: 5000,
         },
@@ -254,24 +291,6 @@ var config = {
       title: '"Geisterhafte" Straßennamen',
       description:
         "Auch Berlin kennt Straßen, die auf keinem Briefumschlag landen. Manchmal sind es Wege, die nur der Infrastruktur dienen – Zufahrten, Brücken, Tunnel. Manchmal sind es Relikte der Teilung: Straßen, die in West- und Ostberlin doppelt existierten und nach der Wende zwischen den Verwaltungen verschwanden. Irgendwie schade, dass sie nie das Privileg haben, auf einem Briefumschlag zu stehen.",
-      location: {
-        center: [13.405, 52.52],
-        zoom: 10.5,
-        pitch: 20,
-        bearing: 0,
-      },
-      mapAnimation: "flyTo",
-      rotateAnimation: false,
-      callback: "",
-      onChapterEnter: [],
-      onChapterExit: [],
-    },
-    {
-      id: "geschichte",
-      alignment: "center",
-      hidden: false,
-      description:
-        "Ein (virtueller) Spaziergang durch Berlin mit einem Blick auf die Straßenschilder ist wie das Lesen eines Palimpsests: Immer wieder wurde überschrieben, aber die älteren Schichten scheinen durch. Preußen, Kaiserreich, Weimarer Republik, NS-Diktatur, Kalter Krieg, Wiedervereinigung – jede Epoche hat ihre Spuren hinterlassen. Und nicht immer wurden sie getilgt. <br>Und wir sind Teil dieser Geschichte und prägen die Identität weiter:",
       location: {
         center: [13.405, 52.52],
         zoom: 10.5,
